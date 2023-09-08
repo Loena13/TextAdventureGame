f4c6483e51b51b6b23e271e432e886189fd2f1db
Luna <lunasmedes@gmail.com>
2023-09-08 12:19:53 +0200

    Added the basic start of the game
    Added Game class with values to start up the game

---
+++ Model/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureGame.Model
+{
+    internal class Game
+    {
+        static string CharacterName;
+
+        public static void StartGame()
+        {
+            Console.WriteLine("Adventure Game!");
+            Console.WriteLine("Welcome to Adventure Game! In this game we will be exploring the world of NAME\n\n");
+            NameCharacter();
+        }
+
+        static void NameCharacter()
+        {
+            Console.WriteLine("You will need a name to go on this adventure. What will it be?\n");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Great! Your character is now named " + CharacterName);
+            Console.Clear();
+            GameProgress();
+
+        }
+
+        public static void GameProgress()
+        {
+            Console.WriteLine("You wake up on the floor slowly opening your eyes as you see a stranger hovering above you\n");
+            Dialog("Hey there are you okay?");
+            Console.WriteLine("How do you answer? 1) Im okay, 2) No, 3) Where am I?, 4) Who are you?");
+        }
+
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
-﻿namespace TextAdventureGame
+﻿using TextAdventureGame.Model;
+
+namespace TextAdventureGame
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Adventure Game!");
-            Console.WriteLine("Welcome to Adventure Game! In this game we will be exploring the world of NAME\n\n");
-
-            Console.WriteLine("You will need a name to go on this adventure. What will it be?\n");
-            string CharacterName = Console.ReadLine();
-            Console.WriteLine("Great! Your character is now named " + CharacterName);
-
+            Game.StartGame();
+            Console.ReadKey();
         }
     }
 }
